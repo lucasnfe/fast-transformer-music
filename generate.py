b4b7494f974892906a557d485848cba591361689
Lucas N. Ferreira <lucasnfe@gmail.com>
2021-12-02 15:27:20 -0300

    Implemented stochastic beam search

--- generate.py
+++ generate.py
@@ -72,40 +72,7 @@ class RecurrentMusicGenerator(torch.nn.Module):
 
         return y_hat, memory
 
-def generate_beam_search(model, prime, n, k, temperature):
-    # Process prime sequence
-    memory = None
-    y_hat = []
-    x_hat = []
-
-    prime_len = len(prime)
-    for i in range(prime_len):
-        x_hat.append(prime[i])
-        y_i, memory = model(x_hat[-1], i=i, memory=memory)
-        y_hat.append(y_i)
-
-    # Generate new tokens
-    for i in range(prime_len, prime_len + n):
-        x_hat.append(sample_topp(y_hat[-1]/temperature, p=0.95))
-        y_i, memory = model(x_hat[-1], i=i, memory=memory)
-        y_hat.append(y_i)
-
-    batch_size, seq_length, _ = post.shape
-    log_post = post.log()
-    log_prob, indices = log_post[:, 0, :].topk(k, sorted=True)
-    indices = indices.unsqueeze(-1)
-
-    for i in range(1, seq_length):
-        log_prob = log_prob.unsqueeze(-1) + log_post[:, i, :].unsqueeze(1).repeat(1, k, 1)
-        log_prob, index = log_prob.view(batch_size, -1).topk(k, sorted=True)
-        indices = torch.cat([indices, index.unsqueeze(-1)], dim=-1)
-
-    return indices, log_prob
-
-    return [int(token) for token in x_hat]
-
-
-def sample_topp(y_hat, p, filter_value=-float("Inf")):
+def filter_top_p(y_hat, p, filter_value=-float("Inf")):
     sorted_logits, sorted_indices = torch.sort(y_hat, descending=True)
     cumulative_probs = sorted_logits.softmax(dim=-1).cumsum(dim=-1)
 
@@ -120,50 +87,153 @@ def sample_topp(y_hat, p, filter_value=-float("Inf")):
     indices_to_remove = sorted_indices_to_remove.scatter(1, sorted_indices, sorted_indices_to_remove)
     y_hat = y_hat.masked_fill(indices_to_remove, filter_value)
 
-    # Sample from filtered categorical distribution
-    y_hat = torch.softmax(y_hat, dim=1)
-    random_idx = torch.multinomial(y_hat.view(-1), num_samples=1)
-    return random_idx.unsqueeze(0)
+    return y_hat
 
-    return scores
-
-def sample_topk(y_hat, k, filter_value=-float("Inf")):
+def filter_top_k(y_hat, k, filter_value=-float("Inf")):
     # Remove all tokens with a probability less than the last token of the top-k
     indices_to_remove = y_hat < torch.topk(y_hat, k)[0][..., -1, None]
     y_hat = y_hat.masked_fill(indices_to_remove, filter_value)
 
-    # Sample from filtered categorical distribution
-    y_hat = torch.softmax(y_hat, dim=1)
-    random_idx = torch.multinomial(y_hat.view(-1), num_samples=1)
-    return random_idx.unsqueeze(0)
+    return y_hat
 
-def generate(model, prime, n, k, temperature):
+def filter_repetition(previous_tokens, scores, penalty=1.0001):
+    score = torch.gather(scores, 1, previous_tokens)
+
+    # if score < 0 then repetition penalty has to be multiplied to reduce the previous token probability
+    score = torch.where(score < 0, score * penalty, score / penalty)
+
+    scores.scatter_(1, previous_tokens, score)
+    return scores
+
+def sample_tokens(y_hat, num_samples=1):
+    # Sample from filtered categorical distribution
+    probs = torch.softmax(y_hat, dim=1)
+    random_idx = torch.multinomial(probs, num_samples)
+    return random_idx
+
+def generate(model, prime, n, k=0, p=0, temperature=1.0):
     # Process prime sequence
     memory = None
     y_hat = []
     x_hat = []
 
-    prime_len = len(prime)
+    prime_len = prime.shape[1]
     for i in range(prime_len):
-        x_hat.append(prime[i])
+        x_hat.append(prime[:,i])
         y_i, memory = model(x_hat[-1], i=i, memory=memory)
         y_hat.append(y_i)
 
     # Generate new tokens
     for i in range(prime_len, prime_len + n):
-        x_hat.append(sample_topp(y_hat[-1]/temperature, p=0.9))
+        y_i = y_hat[-1]/temperature
+
+        if k > 0:
+            y_i = filter_top_k(y_i, k)
+        if p > 0 and p < 1.0:
+            y_i = filter_top_p(y_i, p)
+
+        x_hat.append(sample_tokens(y_i))
         y_i, memory = model(x_hat[-1], i=i, memory=memory)
         y_hat.append(y_i)
 
     return [int(token) for token in x_hat]
+
+def generate_beam_search(model, prime, n, beam_size, k=0, p=0, temperature=1.0):
+    # Process prime sequence
+    memory = None
+    y_hat = []
+    x_hat = []
+
+    # batch size and number of words
+    batch_size = prime.shape[0]
+
+    # expand to beam size the source latent representations / source lengths
+    prime = prime.unsqueeze(1).expand((batch_size, beam_size) + prime.shape[1:]).contiguous().view((batch_size * beam_size,) + prime.shape[1:])
+
+    prime_len = prime.shape[1]
+    for i in range(prime_len):
+        x_hat.append(prime[:,i])
+        y_i, memory = model(x_hat[-1], i=i, memory=memory)
+        y_hat.append(y_i)
+
+    # Get vocabulary size from logits
+    vocab_size = y_hat[-1].shape[-1]
+
+    # Create first beam
+    scores = torch.nn.functional.log_softmax(y_hat[-1], dim=-1)
+
+    # Apply temperature, top_k and top_p filters
+    scores = scores/temperature
+
+    if k > 0:
+        scores = filter_top_k(scores, k)
+    if p > 0 and p < 1.0:
+        scores = filter_top_p(scores, p)
+
+    first_words = sample_tokens(scores, num_samples=beam_size)[0]
+    first_scores = scores[0][first_words]
+
+    first_scores, _indices = torch.sort(first_scores, descending=True, dim=0)
+    first_words = first_words[_indices]
+
+    beam_scores = first_scores
+    beam_candidates = torch.cat((prime, first_words.view(beam_size, batch_size)), dim=1)
+
+    x_hat.append(first_words.view(-1))
+
+    # current position
+    for i in range(prime_len, prime_len + n):
+        # Compute scores
+        y_i, memory = model(x_hat[-1], i=i, memory=memory)
+        y_hat.append(y_i)
+
+        scores = torch.nn.functional.log_softmax(y_hat[-1], dim=-1)
+        _scores = scores + beam_scores[:, None].expand_as(scores)
+
+        # Apply temperature, top_k and top_p filters
+        _scores = _scores/temperature
+
+        if k > 0:
+            _scores = filter_top_k(_scores, k)
+        if p > 0 and p < 1.0:
+            _scores = filter_top_p(_scores, p)
+
+        _scores = _scores.view(batch_size, beam_size * vocab_size)
+
+        next_words = sample_tokens(_scores, num_samples=beam_size)
+        next_scores = _scores.gather(1, next_words)
+
+        next_scores, _indices = torch.sort(next_scores, descending=True, dim=1)
+        next_words = next_words.gather(1, _indices)
+
+        beam_ids = next_words // vocab_size
+        word_ids = next_words % vocab_size
+
+        # Generate new beam candidates
+        beam_candidates = beam_candidates[beam_ids].squeeze()
+        beam_candidates = torch.cat((beam_candidates, word_ids.view(beam_size, batch_size)), dim=1)
+        beam_scores = next_scores.view(-1)
+
+        print(beam_candidates)
+        print(beam_scores)
+
+        x_hat.append(word_ids.view(-1))
+
+    # Get best candidate
+    best_id = torch.argmax(beam_scores)
+    best_candidate = beam_candidates[best_id]
+
+    print("best_candidate", best_candidate)
+
+    return [int(token) for token in best_candidate]
 
 if __name__ == '__main__':
     # Parse arguments
     parser = argparse.ArgumentParser(description='generate.py')
     parser.add_argument('--model', type=str, required=True, help="Path to load model from.")
     parser.add_argument('--seq_len', type=int, required=True, help="Max sequence to process.")
-    parser.add_argument('--k', type=int, default=10, help="Number k of elements to consider while sampling.")
-    parser.add_argument('--p', type=int, default=1.0, help="Probability p to consider while sampling.")
+    parser.add_argument('--k', type=int, default=0, help="Number k of elements to consider while sampling.")
+    parser.add_argument('--p', type=float, default=1.0, help="Probability p to consider while sampling.")
     parser.add_argument('--t', type=float, default=1.0, help="Sampling temperature.")
     parser.add_argument('--vocab_size', type=int, required=True, help="Vocabulary size.")
     parser.add_argument('--n_layers', type=int, default=4, help="Number of transformer layers.")
@@ -188,10 +258,11 @@ if __name__ == '__main__':
     model.eval()
 
     # Define prime sequence
-    prime = [START_TOKEN,373,67,296,195,372,72,295,200,372,74,294,202,373,76,372,48,296,204,372,60]
-    prime = torch.tensor(prime).unsqueeze(1)
+    prime = [START_TOKEN]
+    prime = torch.tensor(prime).unsqueeze(dim=0)
 
     # Generate continuation
-    piece = generate(model, prime, n=1000, k=opt.k, temperature=opt.t)
+    # piece = generate_beam_search(model, prime, n=1000, beam_size=8, k=opt.k, p=opt.p, temperature=opt.t)
+    piece = generate(model, prime, n=1000, k=opt.k, p=opt.p, temperature=opt.t)
     decode_midi(piece, "results/generated_piece.mid")
     print(piece)
