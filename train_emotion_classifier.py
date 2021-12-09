5b6300ce76d2e162ce3e202e147884466fdfdab2
Lucas N. Ferreira <lucasnfe@gmail.com>
2021-12-09 01:15:14 -0300

    Implemented transformer emotion classifier with transfer learning

--- train_emotion_classifier.py
+++ train_emotion_classifier.py
@@ -1,8 +1,8 @@
 import torch
+import time
 import argparse
 import numpy as np
 
-from radam import RAdam
 from vgmidi import VGMidiLabelled
 
 from sklearn.feature_extraction.text import TfidfTransformer
@@ -10,12 +10,13 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model            import SGDClassifier
 from sklearn.metrics                 import confusion_matrix
 
-from models.music_generator import MusicGenerator
+from models.music_emotion_classifier import MusicEmotionClassifier
 
 def encode_bow(xs, vocab_size):
     # Create bag of words
     vocabulary = {str(i):i for i in range(vocab_size)}
 
+    # Make sure pieces are strings
     count_vect = CountVectorizer(vocabulary=vocabulary)
     xs_count = count_vect.fit_transform(xs)
 
@@ -30,14 +31,11 @@ def evaluate_clf(y_test, y_hat):
     confusion = confusion_matrix(y_test, y_hat)
     return accuracy, confusion
 
-def train_baseline(train_data, test_data):
-    x_train, y_train = train_data
-    y_train, y_test = test_data
-
+def train_baseline(x_train, x_test, y_train, y_test):
     # Fit Logistic Regression
     clf = SGDClassifier(loss="log",
                  penalty="l1",
-                   alpha=1e-3,
+                   alpha=1e-4,
             random_state=42,
                 max_iter=1000,
                      tol=0.001)
@@ -49,11 +47,14 @@ def train_baseline(train_data, test_data):
     print("Accuracy:", accuracy)
     print(confusion)
 
-def train(model, train_data, test_data, epochs, lr):
+def train(model, train_data, test_data, epochs, lr, log_interval=1):
     criterion = torch.nn.CrossEntropyLoss()
-    optimizer = RAdam(model.parameters(), lr=lr, weight_decay=0.01)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
+    total_loss = 0
     for epoch in range(1, epochs + 1):
+        epoch_start_time = time.time()
+
         for batch, (x, y) in enumerate(train_data):
             # Forward pass
             x = x.to(device)
@@ -62,14 +63,57 @@ def train(model, train_data, test_data, epochs, lr):
 
             # Backward pass
             optimizer.zero_grad()
-            loss = criterion(y_hat.view(-1, vocab_size), y.view(-1))
+            loss = criterion(y_hat[:,-1,:], y.view(-1))
             loss.backward()
             optimizer.step()
+
+            # print statistics
+            total_loss += loss.item()
+            if batch % log_interval == 0 and batch > 0:
+                num_batches = len(train_data)
+                cur_loss = total_loss / log_interval
+                print(f'| epoch {epoch:3d} | {batch:5d}/{num_batches:5d} batches | '
+                      f'lr {lr:02.5f} | loss {cur_loss:5.2f}')
+
+                total_loss = 0
+
+        # Evaluate model on test set
+        accuracy = evaluate(model, test_data)
+
+        elapsed = time.time() - epoch_start_time
+
+        # Log training statistics for this epoch
+        print('-' * 89)
+        print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | '
+              f'accuracy {accuracy:5.2f}')
+        print('-' * 89)
+
+def evaluate(model, test_data):
+    model.eval()
+
+    total = 0
+    correct = 0
+    with torch.no_grad():
+        for batch, (x, y) in enumerate(test_data):
+            x = x.to(device)
+            y = y.to(device)
+
+            # Evaluate
+            y_hat = model(x)
+
+            # the class with the highest energy is what we choose as prediction
+            _, y_hat = torch.max(y_hat.data, 1)
+
+            total += y.size(0)
+            correct += (y_hat == y).sum().item()
+
+    return 100 * correct / total
 
 if __name__ == '__main__':
     # Parse arguments
     parser = argparse.ArgumentParser(description='generate.py')
     parser.add_argument('--train', type=str, required=True, help="Path to train data directory.")
+    parser.add_argument('--test', type=str, required=True, help="Path to train data directory.")
     parser.add_argument('--model', type=str, required=True, help="Path to load model from.")
     parser.add_argument('--vocab_size', type=int, required=True, help="Vocabulary size.")
     parser.add_argument('--epochs', type=int, default=100, help="Epochs to train.")
@@ -85,27 +129,36 @@ if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # Load data as a flat tensors
-    vgmidi_dataset = VGMidiLabelled(opt.train, seq_len=opt.seq_len)
+    vgmidi_train = VGMidiLabelled(opt.train, seq_len=opt.seq_len)
+    vgmidi_test = VGMidiLabelled(opt.test, seq_len=opt.seq_len)
 
     # Get training data
-    x_train, x_test, y_train, y_test = vgmidi_dataset.get_emotion_data(remove_neutral=True)
+    x_train = encode_bow(vgmidi_train.get_pieces_txt(), opt.vocab_size)
+    x_test = encode_bow(vgmidi_test.get_pieces_txt(), opt.vocab_size)
 
-    x_train = encode_bow(x_train, opt.vocab_size)
-    x_test = encode_bow(x_test, opt.vocab_size)
+    train_baseline(x_train, x_test, vgmidi_train.labels, vgmidi_test.labels)
 
-    train_baseline((x_train, x_test), (y_train, y_test))
+    # Batchfy flat tensor data
+    train_loader = torch.utils.data.DataLoader(vgmidi_train, batch_size=opt.batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(vgmidi_test, batch_size=opt.batch_size, shuffle=False)
 
     # Build linear transformer
-    model = MusicGenerator(n_tokens=opt.vocab_size,
-                            d_query=opt.d_query,
-                            d_model=opt.d_query * opt.n_heads,
-                            seq_len=opt.seq_len,
-                     attention_type="causal-linear",
-                           n_layers=opt.n_layers,
-                           n_heads=opt.n_heads).to(device)
+    model = MusicEmotionClassifier(n_tokens=opt.vocab_size,
+                                   d_query=opt.d_query,
+                                   d_model=opt.d_query * opt.n_heads,
+                                   seq_len=opt.seq_len,
+                            attention_type="linear",
+                                  n_layers=opt.n_layers,
+                                   n_heads=opt.n_heads).to(device)
 
     # Load model
     model.load_state_dict(torch.load(opt.model, map_location=device)["model_state"])
-    model.eval()
 
-    train(model, (x_train, x_test), (y_train, y_test), opt.epochs, opt.lr)
+    # Lock paramters and reset last l
+    for param in model.parameters():
+        param.requires_grad = False
+
+    # Reset classficiation head to the emotion classficiation problem
+    model.predictor = torch.nn.Linear(model.predictor.in_features, 4)
+
+    train(model, train_loader, test_loader, opt.epochs, opt.lr)
--- vgmidi.py
+++ vgmidi.py
@@ -11,15 +11,11 @@ from sklearn.model_selection import GroupShuffleSplit
 
 class VGMidiEmotion:
     def __init__(self, valence, arousal):
-        assert valence in (-1, 0, 1)
-        assert arousal in (-1, 0, 1)
+        assert valence in (-1, 1)
+        assert arousal in (-1, 1)
 
         self.va = np.array([valence, arousal])
-        self.quad2emotion = {0: "neutral",
-                             1: "happy",
-                             2: "angry",
-                             3: "sad",
-                             4: "calm"}
+        self.quad2emotion = {0: "happy", 1: "angry", 2: "sad", 3: "calm"}
 
     def __eq__(self, other):
         if other == None:
@@ -42,49 +38,15 @@ class VGMidiEmotion:
 
     def get_quadrant(self):
         if self.va[0] == 1 and self.va[1] == 1:
-            return 1
+            return 0
         elif self.va[0] == -1 and self.va[1] == 1:
-            return 2
+            return 1
         elif self.va[0] == -1 and self.va[1] == -1:
-            return 3
+            return 2
         elif self.va[0] == 1 and self.va[1] == -1:
-            return 4
-
-        return 0
-
-    @staticmethod
-    def from_quadrant(quad):
-        if quad == 0:
-            return VGMidiEmotion(0, 0)
-        elif quad == 1:
-            return VGMidiEmotion(1, 1)
-        elif quad == 2:
-            return VGMidiEmotion(-1, 1)
-        elif quad == 3:
-            return VGMidiEmotion(-1,-1)
-        elif quad == 4:
-            return VGMidiEmotion(1,-1)
+            return 3
 
         return None
-
-    @staticmethod
-    def rand():
-        va = np.random.rand(2)
-        return SBBSEmotion(va[0], va[1])
-
-    @staticmethod
-    def load_signals(path):
-        emotion_signals = {}
-        with open(path) as f:
-            emotion_signals = json.load(f)
-
-        # Convert emotion arrays to SBBSEmotion instances
-        for ep,signals in emotion_signals.items():
-            for i in range(len(emotion_signals[ep])):
-                ((valence, arousal), duration) = emotion_signals[ep][i]
-                emotion_signals[ep][i] = (VGMidiEmotion(valence, arousal), duration)
-
-        return emotion_signals
 
 class VGMidiUnlabelled(torch.utils.data.Dataset):
     def __init__(self, pieces_path, seq_len):
@@ -137,62 +99,54 @@ class VGMidiUnlabelled(torch.utils.data.Dataset):
 class VGMidiLabelled(torch.utils.data.Dataset):
     def __init__(self, midi_csv, seq_len):
         self.seq_len = seq_len
-        self.pieces = self._load_pieces(midi_csv)
+        self.pieces, self.labels = self._load_pieces(midi_csv, seq_len)
 
     def __len__(self):
         return len(self.pieces)
 
     def __getitem__(self, idx):
-        return torch.tensor(self.pieces[idx])
+        return torch.tensor(self.pieces[idx]), torch.tensor(self.labels[idx])
 
-    def _load_pieces(self, midi_csv):
-        pieces = {}
+    def _load_txt(self, file_path):
+        loaded_list = []
+        with open(file_path) as f:
+            loaded_list = [int(token) for token in f.read().split()]
+        return loaded_list
 
+    def _load_pieces(self, midi_csv, seq_len):
+        pieces = []
+        labels = []
+
+        csv_dir, csv_name = os.path.split(midi_csv)
         for row in csv.DictReader(open(midi_csv, "r")):
-            filepath = row["midi"]
-            piece_id = row["id"]
-            valence, arousal = int(row["valence"]), int(row["arousal"])
+            piece_path = os.path.join(csv_dir, row["piece"])
 
-            # Form midi filepath
-            piecepath = os.path.join(os.path.dirname(midi_csv), filepath)
+            # Make sure the piece has been encoded into a txt file (see encoder.py)
+            file_name, extension = os.path.splitext(piece_path)
+            if os.path.isfile(file_name + ".txt"):
+                # Load entire piece
+                encoded = self._load_txt(file_name + ".txt")
 
-            # Form txt filepath
-            txt_file = os.path.splitext(piecepath)[0] + ".txt"
-            if os.path.exists(txt_file):
-                emotion = VGMidiEmotion(valence, arousal)
+                # Get emotion
+                emotion = VGMidiEmotion(int(row["valence"]), int(row["arousal"]))
 
-                # Read txt file
-                with open(txt_file) as fp:
-                    tokens = fp.read().splitlines()[0]
+                # Split the piece into sequences of len seq_len
+                for i in range(0, len(encoded), seq_len):
+                    piece_seq = encoded[i:i+seq_len]
+                    if len(piece_seq) < seq_len:
+                        # Pad sequence
+                        piece_seq += [PAD_TOKEN] * (seq_len - len(piece_seq))
 
-                if piece_id not in pieces:
-                    pieces[piece_id] = []
+                    pieces.append(piece_seq)
+                    labels.append(emotion.get_quadrant())
 
-                # Append piece with emotion to the dataset
-                pieces[piece_id].append((tokens, emotion))
+        assert len(pieces) == len(labels)
+        return pieces, labels
+
+    def get_pieces_txt(self):
+        pieces = []
+        for piece in self.pieces:
+            piece_txt = " ".join([str(token) for token in piece])
+            pieces.append(piece_txt)
 
         return pieces
-
-    def get_emotion_data(self, train_size=.7, test_size=.3, remove_neutral=False):
-        xs, ys, groups = [], [], []
-        for group, piece_id in enumerate(sorted(self.pieces)):
-            for phrase in self.pieces[piece_id]:
-                tokens, emotion = phrase
-                emotion = emotion.get_quadrant()
-                if remove_neutral:
-                    if emotion == 0:
-                        continue
-                    emotion -= 1
-
-                xs.append(tokens)
-                ys.append(emotion)
-                groups.append(group)
-
-        # Split data
-        xs, ys, groups = np.array(xs), np.array(ys), np.array(groups)
-        kfold = GroupShuffleSplit(n_splits=1, train_size=train_size, test_size=test_size, random_state=42)
-        for train_index, test_index in kfold.split(xs, ys, groups):
-            x_train, x_test = xs[train_index], xs[test_index]
-            y_train, y_test = ys[train_index], ys[test_index]
-
-        return x_train, x_test, y_train, y_test
