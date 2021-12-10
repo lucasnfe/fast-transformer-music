51f1b13b3647d591180eb79bc0cf3f878378b3a3
Lucas N. Ferreira <lucasnfe@gmail.com>
2021-12-10 15:32:55 -0300

    Cleaned emotion classifier trainer

--- train_emotion_classifier.py
+++ train_emotion_classifier.py
@@ -5,47 +5,7 @@ import numpy as np
 
 from vgmidi import VGMidiLabelled
 
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.linear_model            import SGDClassifier
-from sklearn.metrics                 import confusion_matrix
-
 from models.music_emotion_classifier import MusicEmotionClassifier, MusicEmotionClassifierBaseline
-
-def encode_bow(xs, vocab_size):
-    # Create bag of words
-    vocabulary = {str(i):i for i in range(vocab_size)}
-
-    # Make sure pieces are strings
-    count_vect = CountVectorizer(vocabulary=vocabulary)
-    xs_count = count_vect.fit_transform(xs)
-
-    # Compute tfidf
-    tfidf_transformer = TfidfTransformer()
-    xs_tfidf = tfidf_transformer.fit_transform(xs_count).toarray()
-
-    return xs_tfidf
-
-def evaluate_clf(y_test, y_hat):
-    accuracy = np.mean(y_test == y_hat)
-    confusion = confusion_matrix(y_test, y_hat)
-    return accuracy, confusion
-
-def train_baseline(x_train, x_test, y_train, y_test):
-    # Fit Logistic Regression
-    clf = SGDClassifier(loss="log",
-                 penalty="l2",
-                   alpha=1e-4,
-            random_state=42,
-                max_iter=1000,
-                     tol=0.001)
-
-    clf.fit(x_train, y_train)
-    y_hat = clf.predict(x_test)
-
-    accuracy, confusion = evaluate_clf(y_test, y_hat)
-    print("Accuracy:", accuracy)
-    print(confusion)
 
 def train(model, train_data, test_data, epochs, lr, log_interval=1):
     model.train()
@@ -139,12 +99,6 @@ if __name__ == '__main__':
     # Load data as a flat tensors
     vgmidi_train = VGMidiLabelled(opt.train, seq_len=opt.seq_len)
     vgmidi_test = VGMidiLabelled(opt.test, seq_len=opt.seq_len)
-
-    # Get training data
-    x_train = encode_bow(vgmidi_train.get_pieces_txt(), opt.vocab_size)
-    x_test = encode_bow(vgmidi_test.get_pieces_txt(), opt.vocab_size)
-
-    train_baseline(x_train, x_test, vgmidi_train.labels, vgmidi_test.labels)
 
     # Batchfy flat tensor data
     train_loader = torch.utils.data.DataLoader(vgmidi_train, batch_size=opt.batch_size, shuffle=True)
