2bb6dcb3b72c7aa857b0fe400a1fb68ec96d925c
Lucas N. Ferreira <lucasnfe@gmail.com>
2021-12-16 17:55:32 -0300

    Use adam

--- train_emotion_classifier.py
+++ train_emotion_classifier.py
@@ -24,7 +24,7 @@ def train(model, train_data, test_data, epochs, lr, save_to):
     best_val_accuracy = 0
 
     criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=lr, weight_decay=0.01)
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
     for epoch in range(1, epochs + 1):
         epoch_start_time = time.time()
