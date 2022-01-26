57467a19f0e1193c343bf8307a4ab0b9af1150b5
Lucas N. Ferreira <lucasnfe@gmail.com>
2022-01-26 15:07:12 -0300

    Train a discriminator to classify real/fake pieces.

---
+++ train_real_classifier.py
@@ -0,0 +1,202 @@
+import torch
+import time
+import argparse
+import numpy as np
+
+from vgmidi import VGMidiLabelled, VGMidiRealFake, VGMidiSampler, pad_collate
+from sklearn.metrics import confusion_matrix
+from models.music_emotion_classifier import MusicEmotionClassifier, MusicEmotionClassifierBaseline
+
+# Reproducitility
+np.random.seed(42)
+torch.manual_seed(42)
+
+def save_model(model, optimizer, epoch, save_to):
+    model_path = save_to.format(epoch)
+    torch.save(
+        dict(
+            model_state=model.state_dict(),
+            optimizer_state=optimizer.state_dict(),
+            epoch=epoch
+        ),
+        model_path)
+
+def train(model, train_data, test_data, epochs, lr, save_to):
+    model.train()
+
+    best_model = None
+    best_val_accuracy = 0
+
+    criterion = torch.nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    for epoch in range(1, epochs + 1):
+        epoch_start_time = time.time()
+
+        # Train model for one epoch
+        train_step(model, train_data, epoch, lr, criterion, optimizer)
+
+        # Evaluate model on test set
+        val_accuracy, confusion = evaluate(model, test_data)
+
+        elapsed = time.time() - epoch_start_time
+
+        # Log training statistics for this epoch
+        print('-' * 89)
+        print(f'| end of epoch {epoch:3d} | time: {elapsed:5.2f}s | '
+              f'accuracy {val_accuracy:5.2f} | best val accuracy: {best_val_accuracy:5.2f} ')
+
+        # Save best model so far
+        if val_accuracy > best_val_accuracy:
+            print(f'Validation accuracy improved from {best_val_accuracy:5.2f} to {val_accuracy:5.2f}.'
+                  f'Saving model to {save_to}.')
+
+            print(confusion)
+
+            best_val_accuracy = val_accuracy
+            save_model(model, optimizer, epoch, save_to)
+
+        print('-' * 89)
+
+def train_step(model, train_data, epoch, lr, criterion, optimizer, log_interval=50):
+    model.train()
+    start_time = time.time()
+
+    total_loss = 0
+    for batch, (x, y) in enumerate(train_data):
+        # Forward pass
+        x = x.to(device)
+        y = y.to(device)
+        y_hat = model(x)
+
+        # Backward pass
+        optimizer.zero_grad()
+        loss = criterion(y_hat.view(-1), y.view(-1))
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        total_loss += loss.item()
+        if batch % log_interval == 0 and batch > 0:
+            log_stats(optimizer, epoch, batch, len(train_data), total_loss, start_time, log_interval)
+            total_loss = 0
+            start_time = time.time()
+
+def log_stats(optimizer, epoch, batch, num_batches, total_loss, start_time, log_interval):
+    # Get current learning rate
+    lr = optimizer.param_groups[0]['lr']
+
+    # Compute duration of each batch
+    ms_per_batch = (time.time() - start_time) * 1000 / log_interval
+
+    # Compute current loss
+    cur_loss = total_loss / log_interval
+
+    print(f'| epoch {epoch:3d} | {batch:5d}/{num_batches:5d} batches | '
+          f'lr {lr:02.5f} | ms/batch {ms_per_batch:5.2f} | '
+          f'loss {cur_loss:5.2f}')
+
+def evaluate(model, test_data):
+    model.eval()
+
+    ys = []
+    ys_hat = []
+
+    with torch.no_grad():
+        for batch, (x, y) in enumerate(test_data):
+            x = x.to(device)
+            y = y.to(device)
+
+            # Evaluate
+            y_hat = model(x)
+
+            # the class with the highest energy is what we choose as prediction
+            _, y_hat = torch.max(y_hat.view(-1, 4).data, dim=1)
+
+            ys += y.tolist()
+            ys_hat += y_hat.tolist()
+
+    accuracy = np.mean(np.array(ys) == np.array(ys_hat))
+    confusion = confusion_matrix(ys, ys_hat)
+
+    return accuracy, confusion
+
+def collate_fn_padd(batch):
+    for example in batch:
+        x,y = example
+        print(x.shape)
+    quit()
+
+if __name__ == '__main__':
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='train_emotion_classifier.py')
+    parser.add_argument('--train', type=str, required=True, help="Path to train data directory.")
+    parser.add_argument('--test', type=str, required=True, help="Path to train data directory.")
+    parser.add_argument('--train_fake', type=str, required=True, help="Path to fake train data directory.")
+    parser.add_argument('--test_fake', type=str, required=True, help="Path to fake test data directory.")
+    parser.add_argument('--pre_trained', type=str, required=False, help="Path to load model from.")
+    parser.add_argument('--vocab_size', type=int, required=True, help="Vocabulary size.")
+    parser.add_argument('--epochs', type=int, default=100, help="Epochs to train.")
+    parser.add_argument('--batch_size', type=int, default=32, help="Batch size.")
+    parser.add_argument('--lr', type=float, default=0.0001, help="Learning rate.")
+    parser.add_argument('--seq_len', type=int, required=True, help="Max sequence to process.")
+    parser.add_argument('--n_layers', type=int, default=4, help="Number of transformer layers.")
+    parser.add_argument('--n_finetune', type=int, default=-1, help="Index of last layer to finetune.")
+    parser.add_argument('--d_query', type=int, default=32, help="Dimension of the query matrix.")
+    parser.add_argument('--n_heads', type=int, default=8, help="Number of attention heads.")
+    parser.add_argument('--prefix', type=int, default=0, help="Traing with different prefix sizes.")
+    parser.add_argument('--save_to', type=str, required=True, help="Set a file to save the models to.")
+    opt = parser.parse_args()
+
+    # Set up torch device
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Load real data
+    train_real_data = VGMidiRealFake(opt.train, opt.seq_len, label=1.0, prefix=opt.prefix)
+    test_real_data = VGMidiRealFake(opt.test, opt.seq_len, label=1.0, prefix=opt.prefix)
+
+    # Load fake data
+    train_fake_data = VGMidiRealFake(opt.train, seq_len=opt.seq_len, label=0.0, prefix=opt.prefix)
+    test_fake_data = VGMidiRealFake(opt.test, seq_len=opt.seq_len, label=0.0, prefix=opt.prefix)
+
+    # Mix real and fake data
+    train_data = torch.utils.data.ConcatDataset((train_real_data, train_fake_data))
+    test_data = torch.utils.data.ConcatDataset((test_real_data, test_fake_data))
+
+    # Batchfy flat tensor data
+    train_loader = torch.utils.data.DataLoader(train_data,
+                             batch_size=opt.batch_size,
+                                sampler=VGMidiSampler(train_data, bucket_size=opt.prefix, max_len=opt.seq_len, shuffle=True),
+                             collate_fn=pad_collate)
+
+    test_loader = torch.utils.data.DataLoader(test_data,
+                                            batch_size=opt.batch_size,
+                                               sampler=VGMidiSampler(test_data, bucket_size=opt.prefix, max_len=opt.seq_len, shuffle=False),
+                                            collate_fn=pad_collate)
+
+    # Build linear transformer
+    model = MusicEmotionClassifier(n_tokens=opt.vocab_size,
+                            d_query=opt.d_query,
+                            d_model=opt.d_query * opt.n_heads,
+                            seq_len=opt.seq_len,
+                     attention_type="linear",
+                           n_layers=opt.n_layers,
+                            n_heads=opt.n_heads).to(device)
+
+    # Load model
+    if opt.pre_trained:
+        print(f'Fine-tuning model {opt.pre_trained}')
+        model.load_state_dict(torch.load(opt.pre_trained, map_location=device)["model_state"])
+
+        # Lock paramters and reset last l
+        for i, layer in enumerate(model.transformer.layers):
+            if i < opt.n_finetune:
+                for param in layer.parameters():
+                    param.requires_grad = False
+
+    # Add classification head
+    model = torch.nn.Sequential(model,
+                                torch.nn.Dropout(0.1),
+                                torch.nn.Linear(opt.vocab_size, 1)).to(device)
+
+    train(model, train_loader, test_loader, opt.epochs, opt.lr, opt.save_to)
--- vgmidi.py
+++ vgmidi.py
@@ -8,45 +8,6 @@ from sklearn.model_selection import GroupKFold
 
 PAD_TOKEN = 390
 
-class VGMidiEmotion:
-    def __init__(self, valence, arousal):
-        assert valence in (-1, 1)
-        assert arousal in (-1, 1)
-
-        self.va = np.array([valence, arousal])
-        self.quad2emotion = {0: "q1", 1: "q2", 2: "q3", 3: "q4"}
-
-    def __eq__(self, other):
-        if other == None:
-            return False
-        return (self.va == other.va).all()
-
-    def __ne__(self, other):
-        if other == None:
-            return True
-        return (self.va != other.va).any()
-
-    def __str__(self):
-        return self.quad2emotion[self.get_quadrant()]
-
-    def __getitem__(self, key):
-        return self.va[key]
-
-    def __setitem__(self, key, value):
-        self.va[key] = value
-
-    def get_quadrant(self):
-        if self.va[0] == 1 and self.va[1] == 1:
-            return 0
-        elif self.va[0] == -1 and self.va[1] == 1:
-            return 1
-        elif self.va[0] == -1 and self.va[1] == -1:
-            return 2
-        elif self.va[0] == 1 and self.va[1] == -1:
-            return 3
-
-        return None
-
 def pad_collate(batch):
     max_len = max([example[0].shape[-1] for example in batch])
 
@@ -65,31 +26,6 @@ def pad_collate(batch):
         targets.append(y)
 
     return torch.stack(padded_examples), torch.stack(targets)
-
-class VGMidiSampler(torch.utils.data.Sampler):
-    def __init__(self, data_source, bucket_size=64, max_len=2048, shuffle=False):
-        self.max_len = max_len
-        self.bucket_size = bucket_size
-        self.data_source = data_source
-        self.shuffle = shuffle
-
-    def __iter__(self):
-        buckets = [[] for i in range(0, self.max_len, self.bucket_size)]
-
-        for i, example in enumerate(self.data_source.pieces):
-            bucket_ix = len(example) // self.bucket_size - 1
-            buckets[bucket_ix].append(i)
-
-        idxs = []
-        for i in range(len(buckets)):
-            if self.shuffle:
-                np.random.shuffle(buckets[i])
-            idxs += buckets[i]
-
-        return iter(idxs)
-
-    def __len__(self):
-        return len(self.data_source)
 
 class VGMidiUnlabelled(torch.utils.data.Dataset):
     def __init__(self, pieces_path, seq_len):
@@ -142,15 +78,7 @@ class VGMidiUnlabelled(torch.utils.data.Dataset):
 class VGMidiLabelled(torch.utils.data.Dataset):
     def __init__(self, midi_csv, seq_len, balance=False, prefix=0):
         self.seq_len = seq_len
-        pieces, labels, groups = self._load_pieces(midi_csv, seq_len)
-
-        # Generate prefixes of different sizes
-        if prefix > 0:
-            pieces, labels, groups = self._gen_prefixes(pieces, labels, groups, prefix)
-
-        self.pieces = pieces
-        self.labels = labels
-        self.groups = groups
+        pieces, labels, groups = self._load_pieces(midi_csv, seq_len, prefix)
 
     def __len__(self):
         return len(self.pieces)
@@ -164,7 +92,7 @@ class VGMidiLabelled(torch.utils.data.Dataset):
             loaded_list = [int(token) for token in f.read().split()]
         return loaded_list
 
-    def _load_pieces(self, midi_csv, seq_len):
+    def _load_pieces(self, midi_csv, seq_len, prefix_step=0):
         pieces = []
         labels = []
         groups = []
@@ -179,34 +107,26 @@ class VGMidiLabelled(torch.utils.data.Dataset):
                 # Load entire piece
                 encoded = self._load_txt(file_name + ".txt")
 
-                # Time encoded piece to max len
+                # Trim encoded piece to max len
                 encoded = encoded[:seq_len]
 
                 # Get emotion
                 emotion = VGMidiEmotion(int(row["valence"]), int(row["arousal"]))
 
+                # Add piece
                 pieces.append(encoded)
                 labels.append(emotion.get_quadrant())
                 groups.append(row["game"])
 
+                if prefix_step > 0:
+                    # Generate prefixes of incresing size
+                    for prefix_size in range(prefix_step, len(encoded), prefix_step):
+                        pieces.append(encoded[:prefix_size])
+                        labels.append(emotion.get_quadrant())
+                        groups.append(row["game"])
+
         assert len(pieces) == len(labels) == len(groups)
         return pieces, labels, groups
-
-    def _gen_prefixes(self, xs, ys, groups, prefix_step):
-        # Generate prefixes
-        x_prefixes = []
-        y_prefixes = []
-        groups_prefixes = []
-
-        for x,y,g in zip(xs, ys, groups):
-            for prefix_size in range(prefix_step, len(x) + prefix_step, prefix_step):
-                prefix = list(x[:prefix_size])
-
-                x_prefixes.append(prefix)
-                y_prefixes.append(y)
-                groups_prefixes.append(g)
-
-        return x_prefixes, y_prefixes, groups_prefixes
 
     def get_pieces_txt(self):
         pieces = []
@@ -215,3 +135,110 @@ class VGMidiLabelled(torch.utils.data.Dataset):
             pieces.append(piece_txt)
 
         return pieces
+
+class VGMidiRealFake(torch.utils.data.Dataset):
+    def __init__(self, pieces_path, seq_len, label, prefix=0):
+        self.seq_len = seq_len
+        self.pieces = self._load_pieces(pieces_path, seq_len, prefix)
+        self.label = label
+
+    def __len__(self):
+        return len(self.pieces)
+
+    def __getitem__(self, idx):
+        x = self.pieces[idx]
+        return torch.tensor(x), torch.tensor([self.label])
+
+    def _load_txt(self, file_path):
+        loaded_list = []
+        with open(file_path) as f:
+            loaded_list = [int(token) for token in f.read().split()]
+        return loaded_list
+
+    def _load_pieces(self, pieces_path, seq_len, prefix_step=0):
+        pieces = []
+        for file_path in os.listdir(pieces_path):
+            full_path = os.path.join(pieces_path, file_path)
+            if os.path.isfile(full_path):
+                # Make sure the piece has been encoded into a txt file (see encoder.py)
+                file_name, extension = os.path.splitext(file_path)
+                if extension.lower() == ".txt":
+                    # Load entire piece
+                    encoded = self._load_txt(full_path)
+
+                    # Trim encoded piece to max len
+                    encoded = encoded[:seq_len]
+
+                    # Add piece
+                    pieces.append(encoded)
+
+                    if prefix_step > 0:
+                        # Generate prefixes of incresing size
+                        for prefix_size in range(prefix_step, len(encoded), prefix_step):
+                            pieces.append(encoded[:prefix_size])
+
+        return pieces
+
+class VGMidiEmotion:
+    def __init__(self, valence, arousal):
+        assert valence in (-1, 1)
+        assert arousal in (-1, 1)
+
+        self.va = np.array([valence, arousal])
+        self.quad2emotion = {0: "q1", 1: "q2", 2: "q3", 3: "q4"}
+
+    def __eq__(self, other):
+        if other == None:
+            return False
+        return (self.va == other.va).all()
+
+    def __ne__(self, other):
+        if other == None:
+            return True
+        return (self.va != other.va).any()
+
+    def __str__(self):
+        return self.quad2emotion[self.get_quadrant()]
+
+    def __getitem__(self, key):
+        return self.va[key]
+
+    def __setitem__(self, key, value):
+        self.va[key] = value
+
+    def get_quadrant(self):
+        if self.va[0] == 1 and self.va[1] == 1:
+            return 0
+        elif self.va[0] == -1 and self.va[1] == 1:
+            return 1
+        elif self.va[0] == -1 and self.va[1] == -1:
+            return 2
+        elif self.va[0] == 1 and self.va[1] == -1:
+            return 3
+
+        return None
+
+class VGMidiSampler(torch.utils.data.Sampler):
+    def __init__(self, data_source, bucket_size=64, max_len=2048, shuffle=False):
+        self.max_len = max_len
+        self.bucket_size = bucket_size
+        self.data_source = data_source
+        self.shuffle = shuffle
+
+    def __iter__(self):
+        buckets = [[] for i in range(0, self.max_len, self.bucket_size)]
+
+        for i, example in enumerate(self.data_source):
+            bucket_ix = len(example) // self.bucket_size - 1
+            buckets[bucket_ix].append(i)
+
+        idxs = []
+        for i in range(len(buckets)):
+            if self.shuffle:
+                np.random.shuffle(buckets[i])
+            idxs += buckets[i]
+
+        return iter(idxs)
+
+    def __len__(self):
+        return len(self.data_source)
